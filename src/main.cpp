ae39c10aa45e201d377a0b10ec25316fd8874d1d
rulee.chen <rulee@live.cn>
2022-06-25 12:50:26 +0800

    feat(HomeSpan): try HomeSpan homekit

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,22 @@
 #include <Arduino.h>
 #include <HomeSpan.h>
 
+const char *ssid = "PHICOMM_B618";
+const char *password = "18950098099";
+
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
+  homeSpan.setStatusPin(BUILTIN_LED);
+  homeSpan.setWifiCredentials(ssid, password);
+  homeSpan.setPairingCode("18950098");
+  homeSpan.begin(Category::Switches, "Victor-SWitch");
+  new SpanAccessory();
+  new Service::AccessoryInformation();
+  new Characteristic::Identify();
+  new Service::Switch();
+  new Characteristic::On(true);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  homeSpan.poll();
 }
