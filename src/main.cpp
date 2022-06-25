7d84c0bc1ce49c6b058d1dc803c7bac200645152
rulee.chen <rulee@live.cn>
2022-06-26 00:56:43 +0800

    feat(homekit): test my switch

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,24 @@
 #include <Arduino.h>
 #include <HomeSpan.h>
 
-const char *ssid = "PHICOMM_B618";
-const char *password = "18950098099";
+const char* ssid = "104-2.4";
+const char* password = "18950098099";
+
+struct MySwitch : Service::Switch {
+  int ledPin;
+  SpanCharacteristic* power = nullptr ;
+
+  MySwitch(int ledPin) : Service::Switch() {
+    this->ledPin = ledPin;
+    pinMode(ledPin, OUTPUT);
+    power = new Characteristic::On();
+  }
+
+  bool update() {
+    digitalWrite(ledPin, power->getNewVal());
+    return true;
+  }
+};
 
 void setup() {
   Serial.begin(115200);
@@ -10,11 +26,17 @@ void setup() {
   homeSpan.setWifiCredentials(ssid, password);
   homeSpan.setPairingCode("18950098");
   homeSpan.begin(Category::Switches, "Victor-SWitch");
+
   new SpanAccessory();
-  new Service::AccessoryInformation();
-  new Characteristic::Identify();
-  new Service::Switch();
-  new Characteristic::On(true);
+    new Service::AccessoryInformation();
+      new Characteristic::Identify();
+      new Characteristic::Name("Victor-Switch");
+      new Characteristic::Manufacturer("RuleeSmart");
+      new Characteristic::Model("Victor-Switch-ESP32");
+      new Characteristic::SerialNumber("202206260023");
+      new Characteristic::HardwareRevision("22.1.20");
+      new Characteristic::FirmwareRevision("22.2.30");
+    new MySwitch(BUILTIN_LED);
 }
 
 void loop() {
